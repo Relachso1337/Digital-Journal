8725e5fa1173e89ac8f0462a52a7fc925826f6c5
Relachso1337 <simon.b.kamp@t-online.de>
2024-08-31 21:50:53 +0200

    Projekt ertsellt und Tailwind installiert

---
+++ Digital-Journal-App/App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className='flex-1 justify-center items-center'>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
